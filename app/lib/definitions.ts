f13f47614b903a83480fc34234243f38c927f867
Maksim Vlasiuk <maksym.lang@gmail.com>
2025-09-05 19:10:28 +0200

    fix import

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,5 +1,3 @@
-import {Magic} from "magic-sdk";
-const { Web3 } = require("web3")
 
 export type Suggestion = {
     label: string;
@@ -17,18 +15,8 @@ export interface NavbarSearchProps {
     isLoading?: boolean;
 }
 
-export type MagicContextType = {
-    magic: Magic | null
-    web3: typeof Web3 | null
-}
 
 // Define the type for the user
 export type User = {
     address: string
-}
-
-// Define the type for the user context.
-export type UserContextType = {
-    user: User | null
-    fetchUser: () => Promise<void>
 }
